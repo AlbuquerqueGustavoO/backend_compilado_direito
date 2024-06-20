dd383c6ffa0e6cd65e3d6d98adfa17c9c96a19b0
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2024-06-20 13:05:54 -0300

    corrigindo rota cors

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.json());
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); //https://compiladodeleis.com.br //http://localhost:4200 Corrigido para remover a barra no final
+    res.setHeader('Access-Control-Allow-Origin', 'https://compiladodeleis.com.br'); //https://compiladodeleis.com.br //http://localhost:4200 Corrigido para remover a barra no final
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
