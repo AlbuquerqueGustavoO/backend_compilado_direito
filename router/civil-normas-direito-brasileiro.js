654d342886f9715a9f42ced339af46483b45eda9
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2026-05-25 17:27:12 -0300

    scraping atualizacao modulo civil

--- router/civil-normas-direito-brasileiro.js
+++ router/civil-normas-direito-brasileiro.js
@@ -47,7 +47,7 @@ async function scraping() {
     setTimeout(scraping, timeUntilNextExecution);
 }
 
-//scraping();
+scraping();
 
 civilNormas.get('/', async (req, res) => {
     try {
--- router/civil.js
+++ router/civil.js
@@ -47,7 +47,7 @@ async function scraping() {
     setTimeout(scraping, timeUntilNextExecution);
 }
 
-scraping();
+//scraping();
 
 civil.get('/', async (req, res) => {
     try {
