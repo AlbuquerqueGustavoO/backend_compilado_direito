732c5bedd4ba25ebded0b4be6457b5e7c77b4575
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2026-05-25 17:35:03 -0300

    finalizando modo penal

--- router/penal-organizacao-criminosa.js
+++ router/penal-organizacao-criminosa.js
@@ -47,7 +47,7 @@ async function scraping() {
     setTimeout(scraping, timeUntilNextExecution);
 }
 
-//scraping();
+scraping();
 
 penalOrganizacaoCriminosa.get('/', async (req, res) => {
     try {
