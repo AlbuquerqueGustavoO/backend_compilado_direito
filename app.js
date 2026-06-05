d7370ac4f83abb5dc44589c433a9806446a31da4
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2026-06-04 22:02:02 -0300

    novo testes app

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 
 app.use((req, res, next) => {
-    const allowedOrigins = (process.env.CORS_ALLOWED_ORIGINS || 'http://localhost:4200').split(',').map(s => s.trim());
+    const allowedOrigins = (process.env.CORS_ALLOWED_ORIGINS || 'http://localhost:4200' || 'https://compiladodeleis.com.br').split(',').map(s => s.trim());
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes('*')) {
